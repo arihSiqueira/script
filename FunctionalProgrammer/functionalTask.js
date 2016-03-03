20cd102ba5f7957fb079a9f4250dd43ceb328b11
arihsiqueira <arihane.mariano@gmail.com>
2016-03-03 08:33:45 -0300

    Taks 3

--- FunctionalProgrammer/functionalTask.js
+++ FunctionalProgrammer/functionalTask.js
@@ -34,3 +34,21 @@ contadorObserver.addObserver(listener2);
 
 contadorObserver.executar();
 contadorObserver.executar();
+contadorObserver.executar();
+contadorObserver.executar();
+contadorObserver.executar();
+
+console.log('new Teste');
+console.log('new Teste');
+console.log('new Teste');
+
+
+var contadorObserver2=gerarListener();
+contadorObserver2.addObserver(listener);
+contadorObserver2.addObserver(listener2);
+
+contadorObserver2.executar();
+contadorObserver2.executar();
+contadorObserver2.executar();
+contadorObserver2.executar();
+contadorObserver2.executar();
